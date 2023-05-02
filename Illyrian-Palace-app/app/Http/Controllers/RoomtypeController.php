4853b923ce3ba6960e121750c07d1c868302a4a0
LumHamitii <120671590+LumHamitii@users.noreply.github.com>
2023-05-03 01:33:46 +0200

    FIrst migrations, room type crud
    Ne kete commit kam krijuar modelet e para (room, roomtype) dhe i kam migruar ne databaze, pastaj kam krijuar crudin e pare.

---
+++ Illyrian-Palace-app/app/Http/Controllers/RoomtypeController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Roomtype;
+use Illuminate\Http\Request;
+
+class RoomtypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //shows all data 
+        $data=RoomType::all();
+        return view('roomtype.index',['data'=>$data]); // per mi tregu ne admin page roomtype te insertuara nepermes array 
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //form for adding data 
+        return view('roomtype.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //stores data in databse
+        $data= new Roomtype;
+        $data->title=$request->title;
+        $data->detail=$request->detail;
+        $data->save();
+
+        return redirect('admin/roomtype/create')->with('success', 'Data has been added to the database');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //show specific data
+
+        $data=Roomtype::find($id);
+        return view ('roomtype.show', ['data'=>$data]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit( $id)
+    {
+        //edit data
+       
+
+        $data=Roomtype::find($id);
+        return view ('roomtype.edit', ['data'=>$data]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $data= Roomtype::find($id);
+        $data->title=$request->title;
+        $data->detail=$request->detail;
+        $data->save();
+        return redirect('admin/roomtype/'.$id.'/edit')->with('success', 'Data has been changed');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+       RoomType::where('id',$id)->delete();
+       return redirect('admin/roomtype')->with('success','Data has been deleted.');
+    }
+}
--- Illyrian-Palace-app/routes/web.php
+++ Illyrian-Palace-app/routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomtypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('home', function () {
     return view('home');
 });
+
+// admin dashboard
+
+Route::get('admin', function () {
+    return view('dashboard');
+});
+
+// RoomType Routes
+
+Route::get('admin/roomtype/{id}/delete', [RoomtypeController::class, 'destroy']);
+Route::resource('admin/roomtype', RoomtypeController::class);
+
