cf3c64b1f9daf811eae3d0149bb6957a1b45dd7f
shkelzenii <sk61411@ubt-uni.net>
2023-05-26 15:23:49 +0200

    Crudi per staf

--- Illyrian-Palace-app/app/Http/Controllers/StaffController.php
+++ Illyrian-Palace-app/app/Http/Controllers/StaffController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Staff;
+use App\Models\Department;
 
 class StaffController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        //shows all data 
+        $data=Staff::all();
+        return view('staff.index',['data'=>$data]); 
+
     }
 
     /**
@@ -20,7 +22,10 @@ class StaffController extends Controller
      */
     public function create()
     {
-        //
+        //form for adding data 
+        $departs= Department ::all();
+        return view('staff.create',['departs'=>$departs]);
+
     }
 
     /**
@@ -28,7 +33,17 @@ class StaffController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //stores data in databse
+        $data= new Staff;
+        $imgPath=$request->file('photo')->store('public/imgs');
+        $data->full_name=$request->full_name;
+        $data->department_id=$request->department_id;
+        $data->photo=$imgPath;
+        $data->bio=$request->bio;
+        $data->salary=$request->salary;
+        $data->save();
+
+        return redirect('admin/staff/create')->with('success', 'Data has been added to the database');
     }
 
     /**
@@ -36,16 +51,22 @@ class StaffController extends Controller
      */
     public function show(string $id)
     {
-        //
+        //show specific data
+
+        $data=Staff::find($id);
+        return view ('staff.show', ['data'=>$data]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
+    public function edit($id)
+    {   
+        $departs=Department::all();
+        $data=Staff::find($id);
+        return view('staff.edit',['data'=>$data,'departs'=>$departs]);
     }
+    
 
     /**
      * Update the specified resource in storage.
@@ -53,13 +74,21 @@ class StaffController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $data= Staff::find($id);
+        $data->full_name=$request->full_name;
+        $data->department_id=$request->department_id;
+        $data->bio=$request->bio;
+        $data->salary=$request->salary;
+        $data->save();
+        return redirect('admin/staff/'.$id.'/edit')->with('success', 'Data has been changed');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+       Staff::where('id',$id)->delete();
+       return redirect('admin/staff')->with('success','Data has been deleted.');
     }
 }
